dfd56e1b8f93d52878ea63925bb23803bd101b73
Ana <amsanc001@gmail.com>
2022-01-16 15:06:19 -0500

    check if inputs are empty or pw length incorrect

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,8 +10,42 @@ function generatePassword() {
   var promptUppercase = window.prompt('Do you want uppercase letters? Enter "YES" or "NO" to choose')
   var promptNumeric = window.prompt('Do you want numeric characters? Enter "YES" or "NO" to choose')
   var promptSpecial = window.prompt('Do you want special characters? Enter "YES" or "NO" to choose')
+
+  // check if inputs are empty (validate)
+  while (promptLength === "" || promptLength === null) {
+    alert("You did not input a valid option. Try again!");
+    return "undefined : please answer the password criteria correctly in order to obtain a password 🤪";
+  }
+  // check that the password length is correct
+  while (promptLength < 8 || promptLength > 128) {
+    alert("You did not input a valid option. Try again!");
+    return "undefined : please answer the password criteria correctly in order to obtain a password 🤪";
+  }
+  // check if inputs are empty (validate)
+  while (promptLowercase === "" || promptLowercase === null) {
+    alert("You did not input a valid option. Try again!");
+    return "undefined : please answer the password criteria correctly in order to obtain a password 🤪";
+  }
+  // check if inputs are empty (validate)
+  while (promptUppercase === "" || promptUppercase === null) {
+    alert("You did not input a valid option. Try again!");
+    return "undefined : please answer the password criteria correctly in order to obtain a password 🤪";
+  }
+  // check if inputs are empty (validate)
+  while (promptNumeric === "" || promptNumeric === null) {
+    alert("You did not input a valid option. Try again!");
+    return "undefined : please answer the password criteria correctly in order to obtain a password 🤪";
+  }
+  // check if inputs are empty (validate)
+  while (promptSpecial === "" || promptSpecial === null) {
+    alert("You did not input a valid option. Try again!");
+    return "undefined : please answer the password criteria correctly in order to obtain a password 🤪";
+  }
 }
 
+function querySelector() {
+
+}
 
 // Write password to the #password input
 function writePassword() {

e92ac55f517eabdc1caa6dc043c0e1b876080280
Ana <amsanc001@gmail.com>
2022-01-16 14:34:01 -0500

    pw criteria prompts added

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,6 +4,14 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+function generatePassword() {
+  var promptLength = window.prompt('Input a password length between 8 and 128 characters.')
+  var promptLowercase = window.prompt('Do you want lowercase letters? Enter "YES" or "NO" to choose')
+  var promptUppercase = window.prompt('Do you want uppercase letters? Enter "YES" or "NO" to choose')
+  var promptNumeric = window.prompt('Do you want numeric characters? Enter "YES" or "NO" to choose')
+  var promptSpecial = window.prompt('Do you want special characters? Enter "YES" or "NO" to choose')
+}
+
 
 // Write password to the #password input
 function writePassword() {

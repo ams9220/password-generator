7a8659d3a08f87b0ce75566ad8c07b5213dae789
Ana <amsanc001@gmail.com>
2022-01-16 19:14:25 -0500

    updates

--- assets/js/script.js
+++ assets/js/script.js
@@ -67,7 +67,7 @@ function querySelector() {
   }
 
   else if (promptUppercase === "yes") {
-    promptUppercase.input = (65, 66, 67, 68, 69, 70, 71, 72, 73, 74, 75, 76, 77, 78, 79, 80, 81, 82, 83, 84, 85, 86, 87, 88, 89, 90)
+    promptUppercase.input = (A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W, X, Y, Z)
   }
 
   else if (promptNumeric === "yes") {
@@ -79,7 +79,7 @@ function querySelector() {
   }
 
   for (var i = 0; i < promptLength; i++) {
-    
+
   }
 }
 
